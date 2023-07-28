bbba60ce797c0bd1183bebce207325aa51438469
m41en <male.b.t.2000@gmail.com>
2023-07-27 22:29:02 -0300

    Rutas finales de: posts, votos, comentarios

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\VotesController;
 use App\Http\Controllers\CommentsController;
+use App\Http\Controllers\CharacterizesController;
  
 
 /*
@@ -29,27 +30,27 @@ Route::get('/post/listPost/{id_post}', [PostController::class, 'ListOnePost']);
 Route::post('/post/create', [PostController::class, 'CreatePost']);
 Route::post('/post/delete/{id_post}', [PostController::class, 'Delete']);
 
-
 Route::get('/votes/listAll', [VotesController::class, 'ListAllVotes']);
 Route::get('/votes/listUser/{id_user}', [VotesController::class, 'ListOwnedVotes']);
 Route::get('/votes/listPost/{id_post}', [VotesController::class, 'ListPostVotes']);
 Route::post('/votes/create', [VotesController::class, 'CreateVote']);
 Route::post('/votes/delete/{id_vote}', [VotesController::class, 'Delete']);
 
-
-
-
-
-
-
-
-
-
-
-
-
 Route::get('/comments/listAll', [CommentsController::class, 'ListComments']);
-Route::get('/comments/listOwned/{id_user}', [CommentsController::class, 'ListOwnedComments']);
-Route::get('/comments/listPostComments/{id_post}', [CommentsController::class, 'ListPostComments']);
+Route::get('/comments/listUser/{id_user}', [CommentsController::class, 'ListOwnedComments']);
+Route::get('/comments/listPost/{id_post}', [CommentsController::class, 'ListPostComments']);
 Route::post('/comments/create', [CommentsController::class, 'CreateComment']);
 Route::post('/comments/delete/{id_vote}', [CommentsController::class, 'Delete']);
+
+
+
+
+
+
+
+
+Route::get('/characterizes/listAll', [CharacterizesController::class, 'ListAllCharacterizes']);
+Route::get('/characterizes/listPost/{id_post}', [CharacterizesController::class, 'ListPostLabels']);
+Route::get('/characterizes/listLabel/{id_post}', [CharacterizesController::class, 'ListLabelPosts']);
+
+Route::post('/characterizes/store', [CharacterizesController::class, 'store']);
