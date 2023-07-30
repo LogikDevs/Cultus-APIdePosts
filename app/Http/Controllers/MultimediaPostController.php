2b70625d69374fcaf541f1f472f29922d38e1b83
m41en <male.b.t.2000@gmail.com>
2023-07-30 20:14:53 -0300

    Controlador de multimedia (probado sin imagenes reales), controlador de posts modificado en una linea

--- app/Http/Controllers/MultimediaPostController.php
+++ app/Http/Controllers/MultimediaPostController.php
@@ -3,36 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Models\MultimediaPost;
+use App\Models\Post;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class MultimediaPostController extends Controller
 {
 
-    /*
-//File Upload Function
-public function SaveImage(Request $request) {
-      //check file
-      if ($request->hasFile('image')) {
-            $file = $request->file('image');
-            $filename = $file->getClientOriginalName();
-            $extension = $file->getClientOriginalExtension();
-            $picture = date('His').'-'.$filename;
-            //move image to public/img folder
-            $file->move(public_path('img'), $picture);
-            return response()->json(["message" => "Image Uploaded Succesfully"]);
-      } 
-      else
-      {
-            return response()->json(["message" => "Select image first."]);
-      }
+    public function ListAll(Request $request) {
+        return MultimediaPost::all();
     }
-    */
 
-    public function store(Request $request) {
+    public function ListMultimediaPost ($id_post) {
+        return MultimediaPost::where('fk_id_post', $id_post)->get();
+    }
+
+
+
+
+
+
+
+
+    public function SaveMultimedia (Request $request) {
         $this->validate($request, [
-            'fk_id_post' => 'required|exists:posts,id',
-            'multimedia_file' => 'required|file|mimes:jpeg,png,mp4|max:2048', // Asumiendo que solo permitimos imágenes y videos con un tamaño máximo de 2MB
+            'fk_id_post' => 'required | exists:posts,id_post',
+            'multimedia_file' => 'required | file | mimes:jpeg,png,mp4 | max:2048', // Asumiendo que solo permitimos imágenes y videos con un tamaño máximo de 2MB
         ]);
 
         $mediaPost = new MultimediaPost();
@@ -40,15 +36,27 @@ public function SaveImage(Request $request) {
 
         // Subir el archivo multimedia al servidor y obtener su ruta en el sistema de archivos
         $file = $request->file('multimedia_file');
-        $path = $file->store('public/multimedia');
+        $path = $file->store('public/multimedia_post');
 
         // Almacenar la ruta en la base de datos
-        $mediaPost->multimedia_link = $path;
+        $mediaPost->multimediaLink = $path;
         $mediaPost->save();
 
         return response()->json($mediaPost, 201);
     }
 
+
+
+
+
+
+
+
+
+
+
+
+/*
     public function update(Request $request, $id) {
         $this->validate($request, [
             'fk_id_post' => 'exists:posts,id_post',
@@ -65,23 +73,45 @@ public function SaveImage(Request $request) {
         if ($request->hasFile('multimedia_file')) {
             $file = $request->file('multimedia_file');
             $path = $file->store('public/multimedia');
-            $mediaPost->multimedia_link = $path;
+            $mediaPost->multimediaLink = $path;
         }
 
         $mediaPost->save();
 
         return response()->json($mediaPost);
     }
+*/
 
-    public function destroy($id) {
+
+
+
+
+    public function Delete($id_post) {
+    /*
         $mediaPost = MultimediaPost::findOrFail($id);
 
         // Eliminar el archivo multimedia del servidor
-        if (file_exists(public_path($mediaPost->multimedia_link))) {
-            unlink(public_path($mediaPost->multimedia_link));
+        if (file_exists(public_path($mediaPost->multimediaLink))) {
+            unlink(public_path($mediaPost->multimediaLink));
         }
 
         $mediaPost->delete();
+
+        return response()->json(null, 204);
+    */
+
+
+
+
+
+
+        $post = MultimediaPost::where('fk_id_post', $id_post)->get();
+        // Eliminar el archivo multimedia del servidor
+        if (file_exists(public_path($post->multimediaLink))) {
+            unlink(public_path($post->multimediaLink));
+        }
+
+        $post->delete();
 
         return response()->json(null, 204);
     }
