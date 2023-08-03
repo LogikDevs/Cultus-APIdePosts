8176b3a533bb5ebfa0826ab1139d44a82c1ed873
unknown <male.b.t.2000@gmail.com>
2023-08-03 19:17:40 -0300

    Primer prueba de testing

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -2,32 +2,36 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+//use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class PostTest extends TestCase
 {
+    //use RefreshDatabase;
     public function test_ListUserPostGoodRequest() {
-        $response = $this->get('/api/post/listUser/1');
+        $response = $this->get('/api/post/listUser/4');
         
-        $response -> assertStatus(200);
+        //dd($response->json());
+        //$response -> assertStatus(200);
         $response->assertJsonStructure([
-            "id_post",
-            "fk_id_user",
-            "text",
-            "latitud",
-            "longitud",
-            "date",
-            "votes",
-            "comments",
-            "created_at",
-            "updated_at",
-            "deleted_at"
+            '*' => [
+                "id_post",
+                "fk_id_user",
+                "text",
+                "latitud",
+                "longitud",
+                "date",
+                "votes",
+                "comments",
+                "created_at",
+                "updated_at",
+                "deleted_at"
+            ]
         ]);
         $response -> assertJsonFragment([
-            "id_post"=> 1,
-            "fk_id_user"=> 1,
+            "id_post"=> 9,
+            "fk_id_user"=> 4,
             "deleted_at"=> null
         ]);
     }
