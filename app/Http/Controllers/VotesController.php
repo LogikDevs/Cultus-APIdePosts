a8bd909138011797c1af9f2dacf791e005b7dce7
unknown <male.b.t.2000@gmail.com>
2023-08-11 08:27:02 -0300

    commit to pull

--- app/Http/Controllers/VotesController.php
+++ app/Http/Controllers/VotesController.php
@@ -21,34 +21,6 @@ class VotesController extends Controller
         return Votes::where('fk_id_post', $id_post)->get();
     }
 
-/*
-    public function CreateVote(Request $request) {
-        $fk_id_user = $request->input('fk_id_user');
-        $fk_id_post = $request->input('fk_id_post');
-        $vote = $request->input('vote');
-
-        $post = Post::find($fk_id_post);
-
-
-        $existingVote = $post->votes()->where('fk_id_user', $fk_id_user)->first();
-        if ($existingVote) {
-            $existingVote->vote = $request->input('vote');
-            $existingVote->save();
-        } else {
-            $post->votes()->create([
-                'vote' => $request->input('vote'),
-                'fk_id_user' => $fk_id_user,
-            ]);
-        }
-
-        $votesCount = $post->votes()->where('vote', true)->count() - $post->votes()->where('vote', false)->count();
-        $post->votes = $votesCount;
-        $post->save();
-
-        return response()->json(['message' => 'Voto registrado exitosamente']);
-    }
-*/
-
     public function CreateVote(Request $request) {
         $id_user = $request->input('fk_id_user');
         $id_post = $request->input('fk_id_post');
@@ -59,19 +31,20 @@ class VotesController extends Controller
         $this->UpdateCreateVote($post, $id_user, $vote);
         $this->UpdateVoteCount($post);
     }
-            private function UpdateCreateVote(Post $post, $id_user, $vote) {
-                $existingVote = $post->votes()->where('fk_id_user', $id_user)->first();
+    
+    private function UpdateCreateVote(Post $post, $id_user, $vote) {
+        $existingVote = $post->votes()->where('fk_id_user', $id_user)->first();
 
-                if ($existingVote) {
-                    $existingVote->vote = $vote;
-                    $existingVote->save();
-                } else {
-                    $post->votes()->create([
-                        'vote' => $vote,
-                        'fk_id_user' => $id_user,
-                    ]);
-                }
-            }
+        if ($existingVote) {
+            $existingVote->vote = $vote;
+            $existingVote->save();
+        } else {
+            $post->votes()->create([
+                'vote' => $vote,
+                'fk_id_user' => $id_user,
+            ]);
+        }
+    }
 
     public function Delete(Request $request, $id_vote) {
         $vote = Votes::findOrFail($id_vote);
