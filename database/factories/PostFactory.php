6ba3f996dba797a716e66eeaaea69a82de3c4c11
m41en <male.b.t.2000@gmail.com>
2023-08-14 18:16:42 -0300

    Factory de post

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -5,29 +5,35 @@ namespace Database\Factories;
 use App\Models\User;
 use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Http;
 
 class PostFactory extends Factory
 {
     public function definition()
     {
+        /*
+            return [
+                'fk_id_user' => User::all()->random()->id,
+                'text' => $this->faker->paragraph(),
+                'latitud' => $this->faker->latitude(),
+                'longitud' => $this->faker->longitude(),
+                'date' => $this->faker->dateTime()
+            ];
+        */
+
         $response = Http::get('http://localhost:8003/api/events');
         if ($response->successful()) {
             $events = $response->json();
             $event = collect($events)->random();
             return [
-                'fk_id_event' => $event['id_event'],
+                'fk_id_user' => User::all()->random()->id,
+                'fk_id_event' => $event['id_event'],            'fk_id_user' => User::all()->random()->id,
+                'text' => $this->faker->paragraph(),
+                'latitud' => $this->faker->latitude(),
+                'longitud' => $this->faker->longitude(),
+                'date' => $this->faker->dateTime()
             ];
         } else {
             return [];
         }
-        return [
-            'fk_id_user' => User::all()->random()->id,
-            'fk_id_event' => Events::all()->random()->id,
-            'text' => $this->faker->paragraph(),
-            'latitud' => $this->faker->latitude(),
-            'longitud' => $this->faker->longitude(),
-            'date' => $this->faker->dateTime()
-        ];
     }
 }
