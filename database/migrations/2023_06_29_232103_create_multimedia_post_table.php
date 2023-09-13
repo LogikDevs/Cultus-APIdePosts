f743a06597b7db13a8ca8a6bbc7cdd3bbe132781
m41en <male.b.t.2000@gmail.com>
2023-09-12 21:21:30 -0300

    Clean code de migraciones

--- database/migrations/2023_06_29_232103_create_multimedia_post_table.php
+++ database/migrations/2023_06_29_232103_create_multimedia_post_table.php
@@ -13,8 +13,6 @@ class CreateMultimediaPostTable extends Migration
             $table->id('id_multimediaPost');
             $table->unsignedBigInteger('fk_id_post');
             $table->string('multimediaLink');
-            //el link es unico? porque podria utilizarse el mismo para fotos de dferentes lugares o se suben varias veces?
-            //eso no conviene, pero entonces necesito buscar siempre q se vaya a subir una foto
 
             $table->foreign('fk_id_post')->references('id_post')->on('post');
 
